eb6936a3ba39badd267cc0753e9c64a27e45fb25
Raj <32778886+rk16449@users.noreply.github.com>
2018-04-23 06:04:22 +0100

    changed project structure, added JUnit testing for singleton

---
+++ DesignPatterns/src/main/singleton/SingletonPattern.java
@@ -0,0 +1,8 @@
+package main.singleton;
+
+public class SingletonPattern {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ DesignPatterns/src/test/singleton/TestSingletonPattern.java
@@ -0,0 +1,7 @@
+package test.singleton;
+
+import junit.framework.TestSuite;
+
+public class TestSingletonPattern extends TestSuite {
+
+}
