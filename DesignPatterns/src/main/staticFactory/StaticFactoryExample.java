a1a87b0c00410edfd55ab91d227811ddd9f94882
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-05-03 18:09:20 +0100

    chore: housekeeping

--- DesignPatterns/src/main/staticFactory/StaticFactoryExample.java
+++ DesignPatterns/src/main/staticFactory/StaticFactoryExample.java
@@ -2,12 +2,8 @@ package main.staticFactory;
 
 public class StaticFactoryExample {
 	public static void main(String[] args) {
-		
 		// No need to create an instance of VehicleFactory, just use the static method
-		
 		Vehicle car1 = VehicleFactory.getVehicle("CAR");
 		car1.move();
-		
-		
 	}
 }
