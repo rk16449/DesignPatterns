7a676a40a9c2b80ef9239d74da5f0890a1b36e9e
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-05-03 20:22:56 +0100

    chore: add main method implementing features

--- DesignPatterns/src/assignment/factory/AnimalClient.java
+++ DesignPatterns/src/assignment/factory/AnimalClient.java
@@ -2,6 +2,12 @@ package assignment.factory;
 
 public class AnimalClient {
 	public static void main(String[] args) {
+		AnimalFactory factory = new AnimalFactory();
 		
+		Animal tiger1 = factory.createAnimal("TIGER");
+		tiger1.breath();
+		
+		Animal duck1 = factory.createAnimal("DUCK");
+		duck1.eat();
 	}
 }
